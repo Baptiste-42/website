ad6118b1f18e1ebbf913d772af11016c45457eaa
Baptiste-42 <70596674+Baptiste-42@users.noreply.github.com>
2021-05-17 20:01:38 +0200

    Add files via upload

---
+++ projet_web/index.js
@@ -0,0 +1,13 @@
+var slide = new Array("images/slide1.jpg", "images/slide2.jpg", "images/slide3.jpg", "images/slide4.jpg");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+
+//setInterval("ChangeSlide(1)", 5000);
